a8592d21948a626155102a6a8d5bd1cb98870579
Alexandr Ovsii <sedanvw@gmail.com>
2023-10-15 19:31:41 +0300

    start-classes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+// import { App } from 'components/App';
 import './index.css';
+
+function Test({ name, lastName, price }) {
+  return (
+    <div>
+      <h2>Name: {name}</h2>
+      <h3>Lastname: {lastName}</h3>
+      <p>Price: {price}</p>
+    </div>
+  );
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Test name="Sasha" lastName="Ovsiy" price="500" />
   </React.StrictMode>
 );
